a27b765421255c3147d7d007a283f29f2635caf6
peter <peterloginmanila@gmail.com>
2021-01-17 18:28:38 +0400

    init

---
+++ day01/02var/main.go
@@ -0,0 +1,14 @@
+package main
+
+//声明变量
+//var name string
+//var age int
+//var isOk bool
+
+func main() {
+	name = "理想"
+	age = 16
+//	isOk = true
+
+	fmt.Println("name": %s, "年龄"：%d)
+}
---
+++ day01/helloworld/main.go
@@ -0,0 +1,10 @@
+package main
+
+//导入语句
+import "fmt"
+
+//函数外只能放标识符（变量|常量|函数|类型）
+//程序入口
+func main() {
+	fmt.Println("hello world!")
+}
