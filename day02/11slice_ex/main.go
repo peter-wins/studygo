4517c67527f8f3d26fafa49d21d9d02ea652c28b
peter <peterloginmanila@gmail.com>
2021-01-20 19:01:37 +0400

    Update

---
+++ day02/11slice_ex/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+//1.请写出下面代码的输出结果。
+
+func main() {
+	var a = make([]string, 5, 10)
+	for i := 0; i < 10; i++ {
+		a = append(a, fmt.Sprintf("%v", i))
+	}
+	fmt.Println(a)
+}
