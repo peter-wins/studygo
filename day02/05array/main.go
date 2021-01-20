4c15121e3db963a194dc6a15aedeb28457a77cf2
peter <peterloginmanila@gmail.com>
2021-01-20 11:57:42 +0400

    Update

--- day02/05array/main.go
+++ day02/05array/main.go
@@ -27,4 +27,39 @@ func main() {
 	//3.初始化方式3 根据索引来初始化
 	a3 := [5]int{0: 1, 4: 2}
 	fmt.Println(a3)
+
+	//数组遍历
+	citys := [...]string{"北京", "上海", "深圳"} //索引：0~2 citys[0],citys[1],citys[2]
+	//根据索引遍历
+	for i := 0; i < len(citys); i++ {
+		fmt.Println(citys[i])
+	}
+	// for range遍历
+	for i, v := range citys {
+		fmt.Println(i, v)
+	}
+
+	//多维数组 [[1,2] [3,4] [5,6]]
+	var a11 [3][2]int
+	a11 = [3][2]int{
+		[2]int{1, 2},
+		[2]int{3, 4},
+		[2]int{5, 6},
+	}
+	fmt.Println(a11)
+
+	//多维数组遍历
+	for _, v1 := range a11 {
+		fmt.Println(v1)
+		for _, v2 := range v1 {
+			fmt.Println(v2)
+		}
+	}
+
+	// 数组是值类型
+	b1 := [3]int{1, 2, 3} // [1 2 3]
+	b2 := b1              // [1 2 3]	Ctrl+C 相当于copy
+	b2[0] = 100           // [100 2 3]
+	fmt.Println(b1, b2)
+
 }
