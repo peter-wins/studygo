a9746c6c8c8b6ebf89f678afb9abfcf45788c0db
peter <peterloginmanila@gmail.com>
2021-04-05 14:29:35 +0400

    Update

--- day09/http_server/main.go
+++ day09/http_server/main.go
@@ -1,14 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 // net/http server
 func f1(w http.ResponseWriter, r *http.Request) {
-	str := "<h4> Hello world! </h4>"
-	w.Write([]byte(str))
+	// str := `<h1 style="color:green">Hello 世界!</h1>`
+	// w.Write([]byte(str))
+	b, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		w.Write([]byte(fmt.Sprintf("%v", err)))
+	}
+	w.Write(b)
 }
 
 func main() {
-	http.HandleFunc("/test", f1)
+	http.HandleFunc("/test/", f1)
 	http.ListenAndServe("127.0.0.1:9090", nil)
 }
