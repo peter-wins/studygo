736eabefcf064fb500bd58c5f0933afda7230bb6
peter <peterloginmanila@gmail.com>
2021-04-12 18:09:06 +0400

    Update

--- logagent/main.go
+++ logagent/main.go
@@ -1,4 +1,5 @@
 package main
+
 // logAgent 入口程序
 import (
 	"fmt"
@@ -8,14 +9,16 @@ import (
 	"gopkg.in/ini.v1"
 	"time"
 )
+
 var (
 	cfg = new(conf.AppConf)
 )
-func run (){
+
+func run() {
 	//1.读取日志
 	for {
 		select {
-		case line := <- taillog.ReadChan():
+		case line := <-taillog.ReadChan():
 			//2.发送到Kafka
 			kafka.SendToKafka(cfg.KafkaConf.Topic, line.Text)
 		default:
@@ -23,24 +26,24 @@ func run (){
 		}
 	}
 }
-func main(){
+func main() {
 	//0、加载配置文件
-	err := ini.MapTo(cfg,"conf/config.ini")
-	if err !=nil {
+	err := ini.MapTo(cfg, "conf/config.ini")
+	if err != nil {
 		fmt.Printf("load ini failed, err%v\n", err)
 		return
 	}
-	fmt.Println("加载配置文件成功！")
 	//1、初始化Kafka连接
 	err = kafka.Init([]string{cfg.KafkaConf.Address})
-	if err != nil{
+	if err != nil {
 		fmt.Printf("init Kafka failed, err%v\n", err)
 		return
 	}
 	fmt.Println("Init kafka success.")
 	//2、打开日志文件准备收集日志
-	err = taillog.Init(cfg.TaillogConf.FileName)
-	if err != nil{
+	err = taillog.Init(cfg.PathConf.Logjam)
+	fmt.Printf("打印配置:%v\n", cfg.PathConf.Logjam)
+	if err != nil {
 		fmt.Printf("Init taillog failed, err%v\n", err)
 		return
 	}
