770496b1577d9a6dc499c2bdd78e6d316454ffc8
peter <peterloginmanila@gmail.com>
2021-04-16 10:23:38 +0400

    Update

--- gin/chapter02/chapter02.go
+++ gin/chapter02/chapter02.go
@@ -17,25 +17,49 @@ func User (ctx *gin.Context){
 	// 初始化结构体的两种方式
 	//user_info := UserInfo{Id: 1, Name: "佳佳", Age: 19, Addr: "wewstburry"}
 
-	var user_info UserInfo
-	user_info.Id = 2
-	user_info.Name = "十八岁的小姐姐"
-	user_info.Age = 18
-	user_info.Addr = "荷兰"
+	var person UserInfo
+	person.Id = 2
+	person.Name = "十八岁的小姐姐"
+	person.Age = 18
+	person.Addr = "荷兰"
 
-	ctx.HTML(http.StatusOK,"chapter02/user.html", user_info)
+	ctx.HTML(http.StatusOK,"chapter02/user.html", person)
 }
 // ArrController 渲染数组
 func ArrController(ctx *gin.Context){
 	arr := [3]int{1,3,4}	// 定义一个数组
 	ctx.HTML(http.StatusOK,"chapter02/arr.html", arr)
 }
-// 结构体 数组渲染
+// ArrStruct 结构体数组渲染
 func ArrStruct(ctx *gin.Context){
-	arr_struct := [3]UserInfo{
+	arrStruct := [3]UserInfo{
 		{Id: 1, Name: "娜娜", Age: 19, Addr: "新疆"},
 		{Id: 2, Name: "莉莉", Age: 29, Addr: "上海"},
 		{Id: 3, Name: "等等", Age: 30, Addr: "北京"},
 	}
-	ctx.HTML(http.StatusOK,"chapter02/arr_struct.html", arr_struct)
+	ctx.HTML(http.StatusOK,"chapter02/arr_struct.html", arrStruct)
+}
+
+// MapController map 渲染
+func MapController(ctx *gin.Context){
+	m1 := map[string]string{"name": "玥儿", "age": "19"}
+	m2 := map[string]int{"age": 20}
+	m3 := map[string]interface{}{"m1":m1, "m2":m2}
+	ctx.HTML(http.StatusOK, "chapter02/map.html", m3)
+}
+
+// MapStruct map+struct渲染
+func MapStruct(ctx *gin.Context){
+	mapStruct := map[string]UserInfo{
+		"p1":{Id: 1, Name: "嬛嬛", Age: 29, Addr: "上古石窟"},
+		"p2":{Id: 2, Name: "九九", Age: 18, Addr: "台北花园"},
+		"p3":{Id: 3, Name: "龙盛", Age: 26, Addr: "青斑石门"},
+		"p4":{Id: 4, Name: "吉吉国王", Age: 39, Addr: "原始森林"},
+	}
+	ctx.HTML(http.StatusOK,"chapter02/map_struct.html", mapStruct )
+}
+// slice渲染
+func SliceController (ctx *gin.Context){
+	s1 := []int{1,2,3,4,5,6,7,8}
+	ctx.HTML(http.StatusOK,"chapter02/slice.html",s1)
 }
--- gin/main.go
+++ gin/main.go
@@ -13,13 +13,16 @@ func main(){
 	r:=gin.Default()
 
 	r.LoadHTMLGlob("template/**/*")	// 二级目录的模板指定
-	//r.LoadHTMLGlob("template/**/**/*")	// 三级目录的模板指定
+	//r.LoadHTMLGlob("template/**/**/*")	// 三级目s录的模板指定
 	//r.LoadHTMLFiles("template/index.html")
 
 	r.GET("/", chapter01.Hello)
 	r.GET("/user", chapter02.User)
 	r.GET("arr", chapter02.ArrController)
 	r.GET("/arr_struct",chapter02.ArrStruct)
+	r.GET("/map", chapter02.MapController)
+	r.GET("/map_struct", chapter02.MapStruct)
+	r.GET("/slice", chapter02.SliceController)
 
 	err := r.Run(":8080")
 	if err != nil {
